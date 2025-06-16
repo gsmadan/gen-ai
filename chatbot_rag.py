300ab39fac1fd3d6591e5d1dc872deb43e45a58c
gsmadan <83395367+gsmadan@users.noreply.github.com>
2025-06-16 13:03:06 +0530

    Update chatbot_rag.py

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import RetrievalQA
+# from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+
 
 # Load .env for API key
 load_dotenv()
@@ -11,7 +14,7 @@ api_key = os.getenv("GEMINI_API_KEY")
 
 # Set up the same embedding model used during indexing
 embedding = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004",  # ✅ matches FAISS index
+    model="models/text-embedding-004",  # ✅ matches FAISS inde
     google_api_key=api_key
 )
 
@@ -28,11 +31,20 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.4
 )
 
-# Create RetrievalQA chain (RAG pipeline)
-qa_chain = RetrievalQA.from_chain_type(
+# # Create RetrievalQA chain (RAG pipeline)
+# qa_chain = RetrievalQA.from_chain_type(
+#     llm=llm,
+#     retriever=vectorstore.as_retriever(),
+#     return_source_documents=True
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+qa_chain = ConversationalRetrievalChain.from_llm(
     llm=llm,
     retriever=vectorstore.as_retriever(),
-    return_source_documents=True
+    memory=memory
+    # ,
+    # return_source_documents=True,
+    # output_key="answer"
 )
 
 # Interactive CLI chatbot
@@ -44,8 +56,7 @@ while True:
         break
 
     try:
-        response = qa_chain(user_input)
-        answer = response["result"]
-        print(f"\nGemini: {answer}")
+        response = qa_chain.invoke({"question": user_input})
+        print("\nGemini:", response["answer"])
     except Exception as e:
         print(f"\n[Error] {e}")
